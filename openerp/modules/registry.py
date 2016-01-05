b39bc6ad65675bfc006a3e3025c60df547edc2af
Davor Bojkić <Davor.Bojkic@Storm.hr>
2016-01-05 09:57:12 +0100

    account_reference : labela polja model na pogledu dnevnika

--- openerp/modules/registry.py
+++ openerp/modules/registry.py
@@ -44,7 +44,7 @@ class Registry(Mapping):
         # special cursor for test mode; None means "normal" mode
         self.test_cr = None
 
-        # Indicates that the registry is 
+        # Indicates that the registry is
         self.ready = False
 
         # Inter-process signaling (used only when openerp.multi_process is True):
@@ -234,7 +234,7 @@ class Registry(Mapping):
             cr.execute("""SELECT nextval('base_registry_signaling')""")
             cr.execute("""CREATE SEQUENCE base_cache_signaling INCREMENT BY 1 START WITH 1""")
             cr.execute("""SELECT nextval('base_cache_signaling')""")
-        
+
         cr.execute("""
                     SELECT base_registry_signaling.last_value,
                            base_cache_signaling.last_value
