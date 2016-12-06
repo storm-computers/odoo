82ace45bea0a0fce8f9ed11b6c5d7173205974e3
Davor Bojkić <Davor.Bojkic@Storm.hr>
2016-12-06 13:52:26 +0100

    merge process

--- addons/mrp/report/__init__.py
+++ addons/mrp/report/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import bom_structure
-import mrp_report
-import bom_cost
+import mrp_bom_cost_report
+import mrp_bom_structure_report
